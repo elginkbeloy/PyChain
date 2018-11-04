4c31ab3230e86e4048683089533497efa96f283c
Elgin Beloy <elgin.beloy@cohereum.com>
2018-11-04 12:31:50 -0800

    Added time attribute to Block class.

--- block.py
+++ block.py
@@ -1,6 +1,7 @@
 # Standard imports
 import json
 from hashlib import sha256
+from time import time
 
 class Block():
 	'''
@@ -8,6 +9,7 @@ class Block():
 		*id (int) representing block number, indexing at zero.
 		*ledger (dict/JSON) representing the info the block stores (transactions here).
 		*prev_hash (str) the SHA256 hash of the last block in the chain.
+		*time (UNIX Timestamp / str) unix timestamp from block creation date/time.
 		*difficulty (int) x-number of zeros that must start the block hash.
 		*hash (hex str) the current block's SHA256 hash (of all block attributes).
 
@@ -15,10 +17,11 @@ class Block():
 	Similar to bitcoin, it stores transactions in its ledger.
 	'''
 
-	def __init__(self, id, ledger, prev_hash, difficulty, nonce=0):
+	def __init__(self, id, ledger, prev_hash, time, difficulty, nonce=0):
 		self.id = id
 		self.ledger = ledger
 		self.prev_hash = prev_hash
+		self.time = time
 		self.difficulty = difficulty
 		self.nonce = nonce
 		self.hash = None
@@ -29,17 +32,18 @@ class Block():
 ID: {0}
 Ledger: {1}
 Previous Hash: {2}
-Difficulty: {3}
-Nonce: {4}
-Hash: {5}
-			""").format(self.id, self.ledger, self.prev_hash, self.difficulty,
-				self.nonce, self.hash)
+Creation Date: {3}
+Difficulty: {4}
+Nonce: {5}
+Hash: {6}
+			""").format(self.id, self.ledger, self.prev_hash, self.time,
+			self.difficulty, self.nonce, self.hash)
 
 	# Get a block SHA256 hash based on all hashable block attributes that starts
 	# with current_difficulty(int) number of zeros.
 	def mine_valid_block_hash(self):
 		# SHA256 of all hashable block attributes.
-		block_str = str(self.id) + json.dumps(self.ledger) + self.prev_hash
+		block_str = str(self.id) + json.dumps(self.ledger) + self.prev_hash + str(self.time)
 		valid_hash = sha256((block_str + str(self.nonce)).encode('utf-8')).hexdigest()
 
 		# Increment nonce while sha256 hash does not start with
--- pychain.py
+++ pychain.py
@@ -1,5 +1,6 @@
 # Standard imports
 from hashlib import sha256
+from time import time
 
 # PyChain local imports
 from block import Block
@@ -8,8 +9,9 @@ from chain import Chain
 # Create genesis block to start chain. Include first tx.
 GENESIS_PREV_HASH = sha256('Long live PyChain!'.encode('utf-8')).hexdigest()
 GENESIS_TX = {'to': 'elgin', 'from': '0000', 'amount': 100000}
-GENESIS_DIFFICULTY = 3
-GENESIS_BLOCK = Block(0, GENESIS_TX, GENESIS_PREV_HASH, GENESIS_DIFFICULTY)
+GENESIS_DIFFICULTY = 4
+GENESIS_TIME = time() # This could be hardcoded for consistency.
+GENESIS_BLOCK = Block(0, GENESIS_TX, GENESIS_PREV_HASH, time(), GENESIS_DIFFICULTY)
 
 # Mine GENESIS_BLOCK hash.
 GENESIS_BLOCK.mine_valid_block_hash()
